d762c12796236f3bf2b5de67be1339628d79d4e4
adilstudentbcn <adil.student.bcn@gmail.com>
2026-03-09 17:35:41 +0100

    test2

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -6,8 +7,9 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  js.configs.recommended, 
+  ...tseslint.configs.recommended, 
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
